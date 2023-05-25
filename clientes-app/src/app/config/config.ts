76bbe76c9900c67f202835a690d34982eaf93417
dscifo <diego.scifo@globallogic.com>
2023-05-25 08:26:10 -0300

    add backend local url

--- clientes-app/src/app/config/config.ts
+++ clientes-app/src/app/config/config.ts
@@ -1,1 +1,2 @@
-export const URL_BACKEND = 'https://spring-boot-backend-apirest.herokuapp.com';
+//export const URL_BACKEND = 'https://spring-boot-backend-apirest.herokuapp.com';
+export const URL_BACKEND = 'http://localhost:8080';
