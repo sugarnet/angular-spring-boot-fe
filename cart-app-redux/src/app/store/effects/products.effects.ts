23320bc13370660cbb99398995f1713fead8c08d
Diego Scifo <diegoscifo@gmail.com>
2024-06-29 08:08:53 -0300

    Corregir ProductsEffects y registración en config

--- cart-app-redux/src/app/store/effects/products.effects.ts
+++ cart-app-redux/src/app/store/effects/products.effects.ts
@@ -1,24 +1,20 @@
-import { Injectable } from "@angular/core";
-import { Actions, createEffect, ofType } from "@ngrx/effects";
-import { exhaustMap } from "rxjs";
-import { ProductService } from "../../services/product.service";
-import { findAll, load } from "../products.actions";
+import { Injectable } from '@angular/core';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { EMPTY, catchError, exhaustMap, map } from 'rxjs';
+import { ProductService } from '../../services/product.service';
+import { findAll, load } from '../products.actions';
 
 @Injectable()
 export class ProductsEffects {
+  loadProduct$ = createEffect(
+    () => this.actions$.pipe(
+        ofType(load),
+        exhaustMap(() => this.service.findAll())
+    ).pipe(
+        map(products => findAll({ products })),
+        catchError(() => EMPTY)
+    )
+  );
 
-    loadProduct$ = createEffect(
-        () => this.actions$.pipe(
-            ofType(load), 
-            exhaustMap(() => this.service.findAll()))
-            .pipe(
-                map(products => (findAll({ products })))
-            )
-    );
-
-    constructor(private actions$: Actions, private service: ProductService) {
-
-    }
-
-
+  constructor(private actions$: Actions, private service: ProductService) {}
 }
