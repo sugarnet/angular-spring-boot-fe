46414ef38bca6cfaf882f3c2ff0a1077c244f2a5
Diego Scifo <diegoscifo@gmail.com>
2024-08-25 10:25:26 -0300

    Adding styles and links to paginator component

--- user-app-routes/src/app/components/paginator/paginator.component.ts
+++ user-app-routes/src/app/components/paginator/paginator.component.ts
@@ -1,9 +1,10 @@
 import { Component, Input } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-paginator',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './paginator.component.html'
 })
 export class PaginatorComponent {
